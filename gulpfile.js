f7f29cd358ba1924f1336d8d8b966a6e2c48e92a
Fr3ud <oo.pavelko@gmail.com>
2020-06-27 12:57:28 +0300

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 
 gulp.task('hello', function(callback) {
     console.log('Hello World!');
     callback();
-})
+});
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app/"
+        }
+    })
+});
