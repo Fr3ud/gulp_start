ce715172963e4d7ed0b0300f75dadee862624214
Fr3ud <oo.pavelko@gmail.com>
2020-07-03 17:42:25 +0300

    Add some tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const pug = require('gulp-pug');
+const del = require('del');
 
 gulp.task('mantra', function() {
     while('ego') {
@@ -54,6 +55,20 @@ gulp.task('scss', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('copy:img', function() {
+    return gulp.src('./src/img/**/*.*')
+        .pipe(gulp.dest('./build/img/'));
+});
+
+gulp.task('copy:js', function() {
+    return gulp.src('./src/js/**/*.*')
+        .pipe(gulp.dest('./build/js/'));
+});
+
+gulp.task('clean:build', function() {
+    return del('./build');
+});
+
 gulp.task('server', function() {
     browserSync.init({
         server: {
@@ -65,8 +80,15 @@ gulp.task('server', function() {
 gulp.task('watch', function() {
     watch('./src/pug/**/*.pug', gulp.parallel('pug'));
     watch('./src/scss/**/*.scss', gulp.parallel('scss'));
-    // watch('./build/*.html', gulp.parallel(browserSync.reload));
-    // watch('./build/css/**/*.css', gulp.parallel(browserSync.reload));
+    watch('./src/img/**/*.*', gulp.parallel('copy:img'));
+    watch('./src/js/**/*.*', gulp.parallel('copy:js'));
+
+    watch(['./build/img/**/*.*', './build/js/**/*.*'], 
+        gulp.parallel(browserSync.reload));
 });
 
-gulp.task('start', gulp.parallel('pug', 'scss', 'server', 'watch'));
+gulp.task('start', gulp.series(
+    gulp.parallel('clean:build'),
+    gulp.parallel('copy:img', 'copy:js', 'pug', 'scss'), 
+    gulp.parallel('server', 'watch')
+));
